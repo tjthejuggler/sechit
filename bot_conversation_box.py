e425b65799a9bb7f769ff93633023dfd98d6dcb3
tjthejuggler <tjthejuggler@gmail.com>
2023-03-08 12:32:39 +0100

    tkinter conversation box seems to be working

--- bot_conversation_box.py
+++ bot_conversation_box.py
@@ -45,12 +45,14 @@ class BotTalkBox:
 
     def create_widgets(self):
         # create a label widget with a larger font
-        self.label = tk.Label(self.master, text="Enter some text:", font=("Arial", 14))
-        self.label.pack()
+
 
         # create a text box widget with a larger font
-        self.text_box = tk.Text(self.master, height=5, width=30, font=("Arial", 14))
+        self.text_box = tk.Text(self.master, height=5, width=300, font=("Arial", 14))
         self.text_box.pack()
+
+        self.label = tk.Label(self.master, text="Bot:", font=("Arial", 14, 'bold'), justify='left', fg='blue')
+        self.label.pack(anchor='w')
 
         # bind the Return key to the submit_text function
         self.text_box.bind("<Return>", self.submit_text)
@@ -63,33 +65,23 @@ class BotTalkBox:
         self.text_box.see("1.0")
     
         text = self.text_box.get("1.0", "end-1c")
-        print(text, self.bot_game_sum.read(),self.game)
-        self.text_box.delete('1.0', tk.END)
-        self.text_box.focus_set()
+        #print(text, self.bot_game_sum.read(),self.game)
 
-    def conversation_mode(self): #TODO this needs merged into the submit_text function
         possible_player_numbers = [2,3,4,5,6,7,8,9,10]
         if "living_players" in self.game:
             possible_player_numbers = list(self.game["living_players"])
             possible_player_numbers.remove(1)
-        while True:        
-            conversation_input = input("CONVERSATION MODE(enter to switch): Enter your player number, followed by your comment: ")
-            player_number = conversation_input.split()[0] if len(conversation_input.split()) > 1 else ""
-            if conversation_input == "":
-                clear_console_lines(1)
-                break
-            elif player_number.isdigit() and int(player_number) in possible_player_numbers:
-                bot_response = ask_bot("p"+player_number+" says: "+conversation_input.split(None, 1)[1])
-                make_bot_response(bot_response)
-                input("Press Enter to continue...")
-                clear_console_lines(3)
-            else:
-                clear_console_lines(1)
-
+       
+        player_number = text.split()[0] if len(text.split()) > 1 else ""
+        if  player_number.isdigit() and int(player_number) in possible_player_numbers:
+            bot_response = self.ask_bot("p"+player_number+" says: "+text.split(None, 1)[1])
+            self.make_bot_response(bot_response)
+            self.text_box.delete('1.0', tk.END)
+            self.text_box.focus_set()
 
     def randomly_offer_to_let_bot_talk(self):
         self.check_if_bot_wants_to_talk()
-        self.master.after(random.randint(1000, 5000), self.update_label)
+        self.master.after(random.randint(1000, 5000), self.randomly_offer_to_let_bot_talk)
 
     def check_if_bot_wants_to_talk(self):
         if "player_roles" in self.game:
